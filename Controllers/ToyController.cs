07c72a54674d940fca3ae3042e9b096a5438f8a1
Matt Christiansen <mattchristiansen59@gmail.com>
2024-08-30 13:31:50 -0500

    update: cleaned

--- Controllers/ToyController.cs
+++ Controllers/ToyController.cs
@@ -19,21 +19,4 @@ public class ToyController : Controller {
     {
         return View(await _context.Toys.ToListAsync());
     }
-    
-    // GET: Toys/Details/5
-    // public async Task<IActionResult> Details(int? id)
-    // {
-    //     if (id == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //
-    //     var toy = await _context.Toys.FirstOrDefaultAsync(m => m.Id == id);
-    //     if (toy == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //     
-    //     return View(toy);
-    // }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # artificial-playground
+
+- sentiment analysis
